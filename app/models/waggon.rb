f1c67c69b888f44c1a843eddf5e432d561e908b2
Artem Sarkisov <artem.sarkisov@jetbrains.com>
2017-11-06 00:29:13 +0300

    add number validation to waggon

--- app/models/waggon.rb
+++ app/models/waggon.rb
@@ -2,10 +2,15 @@ class Waggon < ApplicationRecord
   belongs_to :train
 
   validates :number, :waggon_type, presence: true
+  before_validation :set_number
 
-  scope :economy, -> { where(type: 'EconomyWaggon') }
-  scope :sleeper, -> { where(type: 'SleeperWaggon') }
-  scope :luxe, -> { where(type: 'LuxeWaggon') }
-  scope :sitting, -> { where(type: 'SittingWaggon') }
-  scope :ordered, -> { order(:number) }
+  scope :economy, -> {where(type: 'EconomyWaggon')}
+  scope :sleeper, -> {where(type: 'SleeperWaggon')}
+  scope :luxe, -> {where(type: 'LuxeWaggon')}
+  scope :sitting, -> {where(type: 'SittingWaggon')}
+  scope :ordered, -> {order(:number)}
+
+  def set_number
+    self.number ||= train.waggons.maximum(:number).to_i + 1
+  end
 end
